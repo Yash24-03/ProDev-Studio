a2d906e4c27dabfd942ae4f273bb0b54f4cc2d39
Yash24-03 <85743100+Yash24-03@users.noreply.github.com>
2024-09-06 21:56:59 +0530

    "Removed splash.png file and updated email address in Hero component"

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
           <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} src="./person.png" alt="" />
         </motion.div>
 
-        <a className={css.email} href="mailto:zainkeepscode@gmail.com">
+        <a className={css.email} href="mailto:yashwillbe@gmail.com">
           yashwillbe@gmail.com
         </a>
 
